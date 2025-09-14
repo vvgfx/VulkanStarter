7141c5e061ca3ed0aeb52d93da1241a1844cb16c
Vishnu Vardan <vishnuvardan662000@gmail.com>
2025-09-14 02:00:41 -0400

    triangle starter code

---
+++ HelloTriangles/main.cpp
@@ -0,0 +1,104 @@
+// #include <vulkan/vulkan.hpp>
+
+#ifdef __INTELLISENSE__
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr uint32_t WIDTH = 1280;
+constexpr uint32_t HEIGHT = 720;
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan()
+    {
+        createInstance();
+        setupDebugMessenger();
+        createSurface();
+        pickPhysicalDevice();
+        createLogicalDevice();
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    void initWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void createSurface()
+    {
+    }
+
+    void createInstance()
+    {
+        
+    }
+
+    void setupDebugMessenger()
+    {
+    }
+
+    void pickPhysicalDevice()
+    {
+    }
+
+    void createLogicalDevice()
+    {
+    }
+
+    //-----------------------------------------------------
+    // class variables here
+    GLFWwindow *window = nullptr;
+    vk::raii::SurfaceKHR surface = nullptr;
+};
+
+int main()
+{
+    HelloTriangleApplication app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
