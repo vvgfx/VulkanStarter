e1804edbdee807b947ab05d055f4f2acd6c3b151
Vishnu Vardan <vishnuvardan662000@gmail.com>
2025-11-20 23:08:01 -0500

    move material system to new file This way I can separate the material system from the engine and directly use it in the rendergraph

---
+++ vulkan-guide/src/MaterialSystem.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "vk_descriptors.h"
+#include "vk_engine.h"
+#include "vk_types.h"
+
+// GLTF Metalllic-Roughness material system.
+struct GLTFMRMaterialSystem
+{
+    MaterialPipeline opaquePipeline;
+    MaterialPipeline transparentPipeline;
+
+    VkDescriptorSetLayout materialLayout;
+
+    struct MaterialConstants
+    {
+        glm::vec4 colorFactors;
+        glm::vec4 metal_rough_factors;
+        // padding, we need it anyway for uniform buffers
+        glm::vec4 extra[14];
+    };
+
+    struct MaterialResources
+    {
+        AllocatedImage colorImage;
+        VkSampler colorSampler;
+        AllocatedImage metalRoughImage;
+        VkSampler metalRoughSampler;
+        VkBuffer dataBuffer;
+        uint32_t dataBufferOffset;
+    };
+
+    DescriptorWriter writer;
+
+    void build_pipelines(VulkanEngine *engine);
+    void clear_resources(VkDevice device);
+
+    MaterialInstance write_material(VkDevice device, MaterialPass pass, const MaterialResources &resources,
+                                    DescriptorAllocatorGrowable &descriptorAllocator);
+};
--- vulkan-guide/src/PBREngine.h
+++ vulkan-guide/src/PBREngine.h
@@ -1,47 +1,12 @@
 #pragma once
 
+#include "MaterialSystem.h"
 #include "rgraph/ComputeBackgroundFeature.h"
 #include "rgraph/RendergraphBuilder.h"
-#include "sgraph/IScenegraph.h"
 #include <memory>
 #include <vk_descriptors.h>
 #include <vk_engine.h>
 #include <vk_types.h>
-
-// GLTF Metalllic-Roughness material system.
-struct GLTFMRMaterialSystem
-{
-    MaterialPipeline opaquePipeline;
-    MaterialPipeline transparentPipeline;
-
-    VkDescriptorSetLayout materialLayout;
-
-    struct MaterialConstants
-    {
-        glm::vec4 colorFactors;
-        glm::vec4 metal_rough_factors;
-        // padding, we need it anyway for uniform buffers
-        glm::vec4 extra[14];
-    };
-
-    struct MaterialResources
-    {
-        AllocatedImage colorImage;
-        VkSampler colorSampler;
-        AllocatedImage metalRoughImage;
-        VkSampler metalRoughSampler;
-        VkBuffer dataBuffer;
-        uint32_t dataBufferOffset;
-    };
-
-    DescriptorWriter writer;
-
-    void build_pipelines(VulkanEngine *engine);
-    void clear_resources(VkDevice device);
-
-    MaterialInstance write_material(VkDevice device, MaterialPass pass, const MaterialResources &resources,
-                                    DescriptorAllocatorGrowable &descriptorAllocator);
-};
 
 class PBREngine : public VulkanEngine
 {
